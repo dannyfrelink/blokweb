dbf085bc2fd8f24a3ec5042c4baf6241e2287033
dannyfrelink <56267084+dannyfrelink@users.noreply.github.com>
2020-09-24 23:01:02 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,12 @@
 
 var hamburgerMenu = document.querySelector('nav div div>img');
 var uitgeklaptMenu = document.querySelector('nav div>ul:last-of-type');
+var laysLogo = document.querySelector('nav>a img');
 
 
 function menuUitklappen () {
     uitgeklaptMenu.classList.toggle('uitklapmenu');
 }
 
-hamburgerMenu.addEventListener('click', menuUitklappen);
 
+hamburgerMenu.addEventListener('click', menuUitklappen);
